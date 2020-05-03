d7caf66b0889b310dc58f581ca71092c424f346d
angelo-truong <59120978+angelo-truong@users.noreply.github.com>
2020-05-03 00:37:51 -0400

    CovTrack fondations
    simulated user movement, added circle that represents a potentially infected area of COVID-19, triggers a warning message if user walks inside the infected circle

--- app/src/main/java/com/example/covtrack/MapsActivity.java
+++ app/src/main/java/com/example/covtrack/MapsActivity.java
@@ -1,19 +1,46 @@
 package com.example.covtrack;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.math.*;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    Location currentLocation;
+    Marker userLocation;
+    MarkerOptions marker;
+    double userLat = 43.6483112;
+    double userLng = -79.39109;
+    CircleOptions circle = new CircleOptions().center(new LatLng(43.65, -79.39)).radius(100).strokeColor(Color.BLACK);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +51,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -38,10 +63,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //radius on toronto
+        LatLng toronto = new LatLng(43, -80);
+        mMap.addCircle(circle);
+       // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toronto, 1.2f));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng userLatLng = new LatLng(userLat, userLng);
+        marker = new MarkerOptions().position(userLatLng).title("Your Location");
+        marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.user));
+        userLocation = mMap.addMarker(marker);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 16.2f));
+        if(inArea(userLatLng,circle)==true){
+            Toast toast = Toast.makeText(this, "WARNING!!! in proximity of someone with COVID-19 symptoms", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+    public boolean inArea(LatLng user, CircleOptions circle){
+        double radius = circle.getRadius();
+        double longitudeC =Math.toRadians(circle.getCenter().longitude);
+        double longitudeU = Math.toRadians(user.longitude);
+        double latitudeC = Math.toRadians(circle.getCenter().latitude);
+        double latitudeU = Math.toRadians(user.latitude);
+        double longitudeDiff = Math.abs(longitudeC-longitudeU);
+        double latitudeDiff = Math.abs(latitudeC-latitudeU);
+        final double EARTH_RADIUS =6371000;
+        //Inner Haversine formula
+        double d1 = Math.pow(Math.sin(latitudeDiff/2),2)
+                + Math.cos(latitudeU)*Math.cos(latitudeC)
+                * Math.pow(Math.sin(latitudeDiff/2),2);
+        //outer Haversine formula
+        double distance = 2*EARTH_RADIUS * Math.asin(Math.sqrt(d1));
+
+        double epsilion = 0.0000001;
+        System.out.println(distance);
+        System.out.println(radius);;
+        if(distance-radius>epsilion){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+            userLocation.remove();
+            userLng = userLng - 0.0005;
+            LatLng userLatLng = new LatLng(userLat, userLng);
+            marker = new MarkerOptions().position(userLatLng).title("Your Location");
+            marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.user));
+            userLocation = mMap.addMarker(marker);
+            if(inArea(userLatLng,circle)==true){
+                Toast toast = Toast.makeText(this, "WARNING!!! in proximity of someone with COVID-19 symptoms", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }else if(keyCode == KeyEvent.KEYCODE_DPAD_RIGHT){
+            userLocation.remove();
+            userLng = userLng + 0.0005;
+            LatLng userLatLng = new LatLng(userLat, userLng);
+            marker = new MarkerOptions().position(userLatLng).title("Your Location");
+            marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.user));
+            userLocation = mMap.addMarker(marker);
+            if(inArea(userLatLng,circle)==true){
+                Toast toast = Toast.makeText(this, "WARNING!!! in proximity of someone with COVID-19 symptoms", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }else if(keyCode == KeyEvent.KEYCODE_DPAD_DOWN){
+            userLocation.remove();
+            userLat = userLat - 0.0005;
+            LatLng userLatLng = new LatLng(userLat, userLng);
+            marker = new MarkerOptions().position(userLatLng).title("Your Location");
+            marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.user));
+            userLocation = mMap.addMarker(marker);
+            if(inArea(userLatLng,circle)==true){
+                Toast toast = Toast.makeText(this, "WARNING!!! in proximity of someone with COVID-19 symptoms", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }else if(keyCode == KeyEvent.KEYCODE_DPAD_UP){
+            userLocation.remove();
+            userLat = userLat + 0.0005;
+            LatLng userLatLng = new LatLng(userLat, userLng);
+            marker = new MarkerOptions().position(userLatLng).title("Your Location");
+            marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.user));
+            userLocation = mMap.addMarker(marker);
+            if(inArea(userLatLng,circle)==true){
+                Toast toast = Toast.makeText(this, "WARNING!!! in proximity of someone with COVID-19 symptoms", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+        return true;
+    }
+
+    public boolean onKeyUp(int keyCode, KeyEvent event){
+        return super.onKeyUp(keyCode, event);
     }
 }
